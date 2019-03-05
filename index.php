cfec625e6656bab57e21db9324139659ba041902
Carlos <carlos.vazquez.unach@gmail.com>
2019-03-05 09:15:12 -0600

    Creación

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include ("includes/header.php")?>
+    <div class="row">
+        <div class="col">   
+            <?php include ("includes/menu.php")?>
+        </div> 
+    </div>  
+<?php include ("includes/footer.php")?>
