6a048d93fb6c5a8f58a9276f86947ff299cbfd2e
Carlos <carlos.vazquez.unach@gmail.com>
2019-03-11 22:50:36 -0600

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -1,5 +1,8 @@
 <?php require_once ("includes/header.php")?> 
-    
+<div class="container">
+    <div class="row">
+    </div>
+</div>
     <hr>
     <p class = "text-center">ElBuenLector &copy; <?= date("Y");?></p>
 <?php require_once ("includes/footer.php")?>
