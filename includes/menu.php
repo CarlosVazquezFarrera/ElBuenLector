ca09ad2e729905050c10b12f7c16ad2ba219b91b
Carlos <carlos.vazquez.unach@gmail.com>
2019-03-12 10:49:43 -0600

    Actualización Login

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light buscador">
-  <a class="navbar-brand" href="">ElBuenLector<span class="sr-only">(current)</span></a>
+  <a class="navbar-brand text-lector" style="color: black";>ElBuenLector</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- inicio.php
+++ inicio.php
@@ -1,6 +1,36 @@
 <?php require_once ("includes/header.php")?> 
 <div class="container">
     <div class="row">
+        <div class="col-12 col-md-4 offset-md-4 ">
+            <form action="" method="post" class = "form-login">
+                <!-- <p class = "mt-3 h4">Iniciar sesión</p> -->
+                
+                <div class = "d-none d-md-block offset-md-3">
+                    <img src ="images/ico.png" width = "150px">
+                </div>
+                
+
+                <!-- <div class="input-group mt-5">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><img src ="images/user.png" width = "20px"></span>
+                    </div> -->
+                    <label for="" class = "mt-5">Email</label>
+                    <input type = "email" placeholder = "Email" class="form-control" required>
+                <!-- </div>     -->
+                
+                
+                <!-- <div class="input-group mt-4">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><img src ="images/pass.png" width = "20px"></span>
+                    </div> -->
+                    <label for="" class = "mt-4">Contraseña</label>
+                    <input type = "password" placeholder = "***********" class="form-control" required>
+                <!-- </div>  -->
+
+
+                <input type = "submit" value = "Iniciar sesión" class="form-control mt-4 login">
+            </form>
+        </div>
     </div>
 </div>
     <hr>
