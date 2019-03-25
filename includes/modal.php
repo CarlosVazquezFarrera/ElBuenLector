a48102e5270f129862b45b10f89d89cfa74ca750
Carlos <carlos.vazquez.unach@gmail.com>
2019-03-24 20:28:05 -0600

    Update Modal

--- includes/modal.php
+++ includes/modal.php
@@ -1,7 +1,7 @@
 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-        <form method="POST" action ="">
+        <form method="POST" action ="" class = "sesion">
             <div class="row py-5 px-5 text-center">
                 <div class="col-12 user-img my-5">
                     <img src = "images/face.png">
