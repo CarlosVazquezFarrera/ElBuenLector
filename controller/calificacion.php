7ce54879b94a92ed0ae348d41082f0b7c927c9bc
Carlos <carlos.vazquez.unach@gmail.com>
2019-04-11 21:24:46 -0500

    Update calificacion.php

--- controller/calificacion.php
+++ controller/calificacion.php
@@ -12,23 +12,19 @@ if ($total["total"] <= 0)
 {
     if ($mysqli->query("INSERT INTO calificacion (id_libro, id_usuario, calificacion) VALUES ( {$_GET["libro"]}, {$_GET["usuario"]}, {$_GET["calificacion"]} )"))
     {
-        echo "si. Insert";
+        echo '<script> 
+            window.history.go(-1);
+        </script>';
     }
 }
 else
 {
     if ($mysqli->query("UPDATE calificacion SET calificacion = {$_GET["calificacion"]} WHERE id_libro = {$_GET["libro"]} AND id_usuario = {$_GET["usuario"]}"))
     {
-        echo "update";
-    }
-
-   
+        echo '<script> 
+                    window.history.go(-1);
+       </script>';
+    }  
 }
-// {
-//     echo '<script> 
-//     {
-//       window.history.go(-1);
-//     }
-//     </script>';
-// }
+
 ?>
