1d6191b128b8d4a0100a40cc645691d13dab8f7d
Carlos <carlos.vazquez.unach@gmail.com>
2019-04-18 21:44:16 -0500

    Perfil

--- perfil.php
+++ perfil.php
@@ -3,16 +3,24 @@ if(isset($_POST["actualizar"]))
 {
     if($_POST["nombre"])
     {
-        $_SESSION["usuario"] = $_POST["nombre"];
-        echo $_POST["nombre"];
+        if($mysqli->query("UPDATE usuario SET nombre = '{$_POST["nombre"]}' WHERE id_usuario = '{$_SESSION["id_usuario"]}'"))
+        {
+            $_SESSION["usuario"] = $_POST["nombre"];
+        }
     }
     if($_POST["apellidos"])
     {
-        echo $_POST["apellidos"];
+        if($mysqli->query("UPDATE usuario SET apellidos = '{$_POST["apellidos"]}' WHERE id_usuario = '{$_SESSION["id_usuario"]}'"))
+        {
+            $_SESSION["apellidos"] = $_POST["apellidos"];
+        }
     }
     if($_POST["correo"])
     {   
-        echo $_POST["correo"];
+        if($mysqli->query("UPDATE usuario SET correo = '{$_POST["correo"]}' WHERE id_usuario = '{$_SESSION["id_usuario"]}'"))
+        {
+            $_SESSION["correo"] = $_POST["correo"];
+        }
     }
     if($_POST["password"])
     {
@@ -21,7 +29,13 @@ if(isset($_POST["actualizar"]))
     if (isset($_FILES["image"]) && !empty($_FILES["image"]["name"]))
     {
         $nombre = $_SESSION["id_usuario"].'.jpg';
-        move_uploaded_file($_FILES["image"]["tmp_name"], "images/users/".$nombre );
+        if(move_uploaded_file($_FILES["image"]["tmp_name"], "images/users/".$nombre ))
+        {
+            if($mysqli->query("UPDATE usuario SET img = '{$nombre}' WHERE id_usuario = '{$_SESSION["id_usuario"]}'"))
+            {
+                $_SESSION["img"] = $nombre;
+            }
+        }
     }
     header("Location:http://localhost/ElBuenLector/perfil.php");
 }
@@ -31,9 +45,9 @@ if(isset($_POST["actualizar"]))
     <div class="col-12">
         <form action="perfil.php" method="post" enctype = multipart/form-data>          
             <img class = "offset-4 ProfilePicture img-circle mr-3 perfil" src = "images/users/<?=$_SESSION["img"]?>" >         
-            <input type = "text" name = "nombre" class = "form-control  mt-3" placeholder = "Nombre">
-            <input type = "text" name = "apellidos" class = "form-control  mt-3" placeholder = "Apellidos">
-            <input type = "email" name = "correo" class = "form-control  mt-3" placeholder = "Correo">
+            <input type = "text" name = "nombre" class = "form-control  mt-3" placeholder = "Nombre" value = "<?=ucwords($_SESSION["usuario"])?>"> 
+            <input type = "text" name = "apellidos" class = "form-control  mt-3" placeholder = "Apellidos" value = "<?=ucwords($_SESSION["apellidos"])?>">
+            <input type = "email" name = "correo" class = "form-control  mt-3" placeholder = "Correo" value = "<?=$_SESSION["correo"]?>">
             <input type = "password" name = "password" class = "form-control  my-3" placeholder = "Actualizar contraseña">
             <input type = "password2" name = "password" class = "form-control  my-3" placeholder = "Confirmar cambio de contraseña">
             <div>
